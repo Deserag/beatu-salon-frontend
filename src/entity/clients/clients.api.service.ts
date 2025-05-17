dd3b9766fd41fbcdbff307cfff20225b0bdcc939
Deserag <pavelkopytov472@gmail.com>
2025-05-17 20:50:10 +0300

    Правки по системе

--- src/entity/clients/clients.api.service.ts
+++ src/entity/clients/clients.api.service.ts
@@ -1,23 +1,23 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { environment } from "environment/environment";
-import { IReqPage } from "../work-page";
-import { Observable } from "rxjs";
-import { IDeleteClient, TResGetClients } from "./clients.interface";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from 'environment/environment';
+import { IReqPage } from '../work-page';
+import { Observable } from 'rxjs';
+import { IDeleteClient, TResGetClients } from './clients.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientApiService {
-      private _http = inject(HttpClient);
-      private _baseURL = environment.apiService;
+  private _http = inject(HttpClient);
+  private _baseURL = environment.apiService;
 
-      getClients(body: IReqPage): Observable<TResGetClients>{
-        return this._http.post<TResGetClients>(`${this._baseURL}clients/list`, body)
-      }
-      deleteClient(userId: string): Observable<IDeleteClient> {
-          return this._http.delete<IDeleteClient>(
-            `${this._baseURL}user/delete-user/${userId}`
-          );
-        }
+  getClients(body: IReqPage): Observable<TResGetClients> {
+    return this._http.post<TResGetClients>(`${this._baseURL}client/list`, body);
+  }
+  deleteClient(userId: string): Observable<IDeleteClient> {
+    return this._http.delete<IDeleteClient>(
+      `${this._baseURL}user/delete-user/${userId}`
+    );
+  }
 }
